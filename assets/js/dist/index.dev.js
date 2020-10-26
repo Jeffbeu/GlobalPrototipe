a9dd9e2ef6d0ecd3925b93ab8afe3c71c9ba43a4
Jeffbeu <56686731+Jeffbeu@users.noreply.github.com>
2020-10-25 22:11:06 -0300

    s

---
+++ assets/js/dist/index.dev.js
@@ -0,0 +1,12 @@
+"use strict";
+
+window.onload = function () {
+  setTimeout(function () {
+    img.classList.add('scale');
+  }, 2000);
+};
+
+var divImg = document.querySelector('ringOne ring', 'ringTwo ring');
+setTimeout(function () {
+  window.location.href = 'home.html';
+}, 3500);
---
+++ assets/js/index.js
@@ -0,0 +1,11 @@
+window.onload = function(){
+
+    setTimeout(() =>{
+        img.classList.add('scale')
+    }, 2000);
+}
+
+const divImg = document.querySelector('ringOne ring','ringTwo ring');
+setTimeout(()=>{
+    window.location.href = 'home.html'
+}, 3500);
